beac337276fce78cb303d0a6412cd923e32befde
Jake Collins <jakcllns@gmail.com>
2020-10-04 21:56:20 -0400

    Working with express

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const http = require('http');
 
+const express = require('express');
+
+const app = express();
+
+const server = http.createServer(app);
+
+const PORT = 3000;
+
+console.log(`Listening on port ${PORT}...`);
+server.listen(3000);
