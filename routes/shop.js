9b066a13c12dbde1055d9a0eb5685e19923fb649
Jake Collins <jakcllns@gmail.com>
2020-10-10 11:41:26 -0400

    Finished converting static html into Pug templates and extended a base layout template

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ const adminData = require('./admin');
 router.get('/',(req, res, next) => {
     const products = adminData.products;
     // res.sendFile(path.join(routeDir,'views','shop.html'));
-    res.render('shop', {prods: products, docTitle: 'Shop'});
+    res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
