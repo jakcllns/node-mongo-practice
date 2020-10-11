615e3fd45caa8f753d82ef6d81bf9a4672815f15
Jake Collins <jakcllns@gmail.com>
2020-10-11 12:45:53 -0400

    Added additional functionality to Admin Products page Setting up to give buttons actions

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,21 +1,26 @@
 const Product = require('../models/product');
 
+const appendPrefix = url => {
+    return ['/admin', url].join('');
+}
+
 exports.getAddProduct = (req,res,next) => {
-    res.render('admin/add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+    res.render('admin/add-product', {pageTitle: 'Add Product', path: appendPrefix(req.url)});
 }
 
 exports.postAddProduct = (req, res, next) => {
     const product = new Product(req.body.title, req.body.imageUrl,req.body.description, req.body.price);
     product.save();
-    res.redirect('/admin/products');  
+    res.redirect(this.appendPrefix('/products'));  
 }
 
 exports.getProducts = (req, res, next) => {
+    console.log(req.url);
     const products = Product.fetchAll(products => {
-        res.render('admin/products', {prods: products, pageTitle: 'Admin Products', path: req.url, hasProducts: products.length >0});
+        res.render('admin/products', {prods: products, pageTitle: 'Admin Products', path: appendPrefix(req.url), hasProducts: products.length >0});
     });
 }
 
 exports.getEditProduct = (req,res,next) => {
-    res.render('admin/edit-product', {pageTitle: 'Edit Product', path: req.url});
+    res.render('admin/edit-product', {pageTitle: 'Edit Product', path: appendPrefix(req.url)});
 }
