cb5248a1bd3259077d6f5922a347a3a989c89bc3
Jake Collins <jakcllns@gmail.com>
2020-10-17 10:27:41 -0400

    Creating Order and OrderItem models to add Checkout functionality

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 const PORT = 3000;
@@ -43,9 +45,11 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
+Order.belongsTo(User);
+Order.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
-
-database.sync().then(result => {
+database.sync({force: false}).then(result => {
     return User.findByPk(1);
     })
     .then(user => {
---
+++ models/order.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+const database = require('../util/database');
+
+const Order = database.define('order',{
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }
+});
+
+module.exports = Order;
